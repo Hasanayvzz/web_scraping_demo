79e9668911423fa617645206999193af06b969aa
Hasanayvzz <ayvazhasan2015@hotmail.com>
2022-08-07 16:15:26 +0300

    first commit

---
+++ lib/kitap.dart
@@ -0,0 +1,9 @@
+class Kitap {
+  String image;
+  String kitapAdi;
+  String yayinEvi;
+  String yazar;
+  String fiyat;
+
+  Kitap(this.image, this.kitapAdi, this.yayinEvi, this.yazar, this.fiyat);
+}
